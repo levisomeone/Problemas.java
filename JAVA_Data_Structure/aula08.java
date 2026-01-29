85c233b356dc155c1a02a55b65a98cfd4d3befdc
Pedro Levi <133606748+levisomeone@users.noreply.github.com>
2026-01-29 11:14:31 -0300

    Add algorithm complexity comments to aula08.java
    Adicionando Anotações para o arquivo de Complexidade de Algoritmos, aula08 Estrutura de Dados JAVA

---
+++ JAVA_Data_Structure/aula08.java
@@ -0,0 +1,12 @@
+//Complexidade de algoritmo
+/*
+    Processamento(tempo), Espaço(Memória_e_Disk)
+    
+    Note O(..):
+
+    Busca Linear: O(N), Proporcional a tempo e memória; 
+    
+    Melhor algoritmo: O(Log n);
+
+    complexidade.pdf https://share.google/ZwKu7KZ0bmXbESKr7
+*/
