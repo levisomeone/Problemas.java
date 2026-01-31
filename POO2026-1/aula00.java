74e7da0b36da39c50c8d99b1c467157439dbcdc2
Pedro Levi <133606748+levisomeone@users.noreply.github.com>
2026-01-31 19:31:45 -0300

    Update aula00.java
    Anotações da aula 00 POO da faculdade ps:(Vou encontrar um melhor jeito de arquivar anotações durante este processo)

--- POO2026-1/aula00.java
+++ POO2026-1/aula00.java
@@ -1,1 +1,21 @@
+##Programação Orientada a Objetos || Introdução a POO
 
+POO é um paradigma baseado no conceito de objetos, estes objetos contem tipos de dados em formato de campos
+, conhecidos como atributos, e tendo códigos como procedimentos e também chamados de "Métodos"; 
+
+Paradigma como formato de estrutura a regra:
+Programming Low Level-> Linear-> Structured-> Modular-> POO
+
+Baseado em modulação  através de código fonte em função; 
+
+
+##POO conceitualmente é uma implementação de softwarte baseado em iterações de unidades de software,
+estás denomidadas objetos.
+
+A análise permite identificar conjuntos de objetos, simulando o mundo que se deseja aumentando propriedades
+que são desejáveis em relação ao programa.
+
+Conceitos como Classe, Construtor, Objeto, Encapsulamento, etc.., provem de Programação Orientada a Objetos
+
+  ## Linguagem JAVA compreende  Compilação, Interpretação e Tradução ;
+  
