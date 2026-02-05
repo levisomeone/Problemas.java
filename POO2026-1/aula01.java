29f33bff80d7b791aee2672735ece7806b8f6885
Pedro Levi <133606748+levisomeone@users.noreply.github.com>
2026-02-05 20:52:10 -0300

    Correct tabuada calculation in aula01.java
    Fix tabuada loop to increment x and print results correctly.

--- POO2026-1/aula01.java
+++ POO2026-1/aula01.java
@@ -5,3 +5,18 @@
   Algaworks: 6 a 11 capitulos;
 */
 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int tabuada = scanner.nextInt();
+      for(int x=0;x<=10;x++){
+        System.out.println("Tabuada do: " + tabuada);
+        System.out.println(x + "x" + tabuada);
+        System.out.println( x * tabuada);
+      }
+      System.out.println(".");
+    }
+}
